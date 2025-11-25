61ccefe4a4592b7600cf4e8e7bf9b81c98000e11
Tiia Pietikäinen <tiiu_maria@hotmail.com>
2025-11-25 09:50:29 +0200

    Added more quotes

--- sanonnat.js
+++ sanonnat.js
@@ -1,8 +1,22 @@
-let sanonnat = ['"Taiteilijat ovat tavattoman herkkiä."', 
-                '"Senkin röyhkimys! Minä olen täyteläinen!"', 
-                '"Takaisin luontoon. Elämme vain kerran. Hiiteen velvollisuudet."', 
-                '"Tuollainen sinäkin olit tuhat vuotta sitten."', 
-                '"Mörkö, olisimme iloisia jos menisit kotiin."'];
+let sanonnat = ['"Taiteilijat ovat tavattoman herkkiä. — Nipsu, Tove Jansson"', 
+                '"Senkin röyhkimys! Minä olen täyteläinen! — Niiskuneiti, Tove Jansson"', 
+                '"Takaisin luontoon. Elämme vain kerran. Hiiteen velvollisuudet. — Muumipappa, Tove Jansson"', 
+                '"Tuollainen sinäkin olit tuhat vuotta sitten. — Tove Jansson, Tuu-tikki"', 
+                '"Mörkö, olisimme iloisia jos menisit kotiin. — Muumimamma, Tove Jansson"', 
+                '"Vahinko etteivät äidit saa lähteä milloin heitä huvittaa ja ruveta nukkumaan ulkosalla. Varsinkin äidit sitä toisinaan tarvitsisivat. — Muumimamma, Tove Jansson"', 
+                '"Minä en jätä huomiseen mitään, minkä voin tehdä tänään. — Pikku Myy, Tove Jansson"', 
+                '"Kyllä on jännittävää elää, ajatteli Muumipeikko. Kaikki voi hetkessä kääntyä ihan ylösalaisin – ilman mitään syytä. — Muumipeikko, Tove Jansson"', 
+                '"Se on kyllä hyvin pieni eläin. Odotahan, niin valaisen sitä. Kaikki näyttää vaarallisemmalta pimeässä, käsitäthän. — Muumipappa, Tove Jansson"', 
+                '"Ikinä ei tule aivan vapaaksi, jos ihailee toista liikaa, sen minä tiedän. — Nuuskamuikkunen, Tove Jansson"', 
+                '"Ajattelen sitä aamulla, se ei mahdu juuri nyt päähäni. — Muumipeikko, Tove Jansson"', 
+                '"Minä en siedä lääppimistä, minä tahdon paljon lahjoja!  — Pikku Myy, Tove Jansson"', 
+                '"Saattaa olla, että kupillinen teetä viinimarjamehun kera voisi pelastaa minut, mutta eihän sitä koskaan tiedä. — Niiskuneiti, Tove Jansson"', 
+                '"Älä ajattele ikäviä asioita, se on minun mottoni! — Niiskuneiti, Tove Jansson"', 
+                '"Maailma on täynnä suuria ihmeitä sille, joka on valmis ottamaan niitä vastaan. — Muumipappa, Tove Jansson"', 
+                '"Eihän sitä aina voi olla ystävällinen ja seurallinen. Ei yksinkertaisesti ehdi. — Nuuskamuikkunen, Tove Jansson"', 
+                '"Sellainen henkilö, joka syö pannukakkua ja hilloa, ei voi olla kauhean vaarallinen. — Pikku Myy, Tove Jansson"', 
+                '"Minä en poimi kukkasia enkä lahjoita kenellekään suklaarasioita. Minä popsin suklaat itse! — Nipsu, Tove Jansson"', 
+                '"Kun minä jotakin jysäytän, niin se ei totisesti ole mikään kamelin pieru! — Noita, Tove Jansson"']
 
 
 module.exports = {sanonnat};
