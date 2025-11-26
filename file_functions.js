a4ce3fae1490b18b3c50706926af8e455bbf6dbf
Tiia Pietikäinen <tiiu_maria@hotmail.com>
2025-11-26 09:52:55 +0200

    Uusi Functio, jolla tallenetaan Uname ja Pword JSONiin

--- file_functions.js
+++ file_functions.js
@@ -1,3 +1,4 @@
+const { json } = require('express/lib/response')
 const fs = require('fs')
 const path = require('path')
 
@@ -6,4 +7,26 @@ function appendToKayttajat(username,password){
     fs.appendFileSync(filePath, `${username}\n${password}\n\n`)
 }
 
-module.exports = {appendToKayttajat}
+function saveUserToJson(username,password){
+    const file = 'users.json'
+    let users = []
+    //jos tiedost on jo olemassa, haetaan olemassa olevat käyttäjät sieltä
+    if (fs.existsSync(file))//tarkistaa onko tiedsto jo olemassa
+    {
+        //jos läytyy, parsitaan tiedosto luettevaan muotoon
+        users = JSON.parse(fs.readFileSync(file))
+    }
+    //lisätään users:iin (tiedostoon users.json) uusi käyttäjä
+    users.push({username,password})
+
+    //tallenetaan tiedostoon users.json tiedosto
+
+    fs.writeFileSync(file,JSON.stringify(users,null,2))
+    console.log("users.json tallennettiin")
+
+    //null = 'replacer', jolla voidaan määrittää mitkä JSON keyt otetaan mukaan. Jos käytetään null, oetaan mukaan kaikki
+    //null tilalla voisi olla vaikka ["username"] tällöin password jätettäisiin pois
+    // 2 = intendaation (esim. voisi olla vaikka 3 tai 4)(pretty-print)
+}
+
+module.exports = {appendToKayttajat, saveUserToJson}
